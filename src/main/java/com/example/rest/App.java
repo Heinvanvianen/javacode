9e9a7214d0f4280f7e6af958e0026237ad67f6fa
Heinvanvianen <40593991+Heinvanvianen@users.noreply.github.com>
2018-06-26 11:11:25 +0200

    Update App.java

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -50,7 +50,7 @@ public class App {
     private static void runGetRequest() {
  
     	// sample URL
-        String url = "http://140.86.15.104:3000/shield/33/45/blue/Heinvanvianen/";
+        String url = "http://140.86.15.104:3000/shield/33/45/blue/Heinvanvianen";
         CloseableHttpResponse response = null;
  
         try {
